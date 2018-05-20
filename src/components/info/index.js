cf30dcae54c58c56c75bc881a7cdc3fa87f5a1fa
Niv Sardi <xaiki@evilgiggle.com>
2018-05-20 15:29:44 -0300

    split out synopsis, and make it expandable
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/info/index.js
+++ src/components/info/index.js
@@ -24,13 +24,63 @@ const InfoLine = ({year, runtime, genres, rating, ...props}) => (
   </div>
 )
 
+class Synopsis extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      expanded: false
+    }
+  }
+
+  toggleExpanded (e) {
+    e.preventDefault()
+
+    this.setState(state => ({
+      expanded: !state.expanded
+    }))
+  }
+
+  render () {
+    const {text, trimLength} = this.props
+    const {expanded} = this.state
+
+    if (text.length < trimLength) {
+      return (
+        <p className='synopsis'> {text} </p>
+      )
+    }
+
+    if (expanded) {
+      return (
+        <p className='synopsis'>
+          {text}
+          <a onClick={this.toggleExpanded.bind(this)} href='#'> LESS…</a>
+        </p>
+      )
+    }
+
+    return (
+      <p className='synopsis'>
+        {text.slice(0, trimLength)}
+        <a onClick={this.toggleExpanded.bind(this)} href='#'> MORE…</a>
+      </p>
+    )
+  }
+}
+
+Synopsis.defaultProps = {
+  text: '',
+  trimLength: 200
+}
+
 const Info = ({id, goBack, title, synopsis, overview, poster, ...props}) => ([
   <Navbar key={`detail-navbar-${id}`} type='content-nav' goBack={goBack} right={<InfoBar />} />,
   <div key={`details-${id}`}className={style.detail}>
     <div className={style.info}>
       <h1>{title}</h1>
       <InfoLine {...props} />
-      <p className='synopsis'> {overview || synopsis} </p>
+      <Synopsis text={overview || synopsis} />
       <PlayButtons {...props} />
     </div>
     <div className={style.cover}>
