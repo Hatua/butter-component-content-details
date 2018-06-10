029e21d8d760edc41a071f9703902219c3f47771
Niv Sardi <xaiki@evilgiggle.com>
2018-06-10 17:35:30 -0300

    add simple icons
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,13 +1,18 @@
 import React from 'react'
 
 import style from './style.styl'
+import Circle from '../../icons/circle.svg'
+import Magnet from '../../icons/magnet.svg'
+import Eye from '../../icons/eye.svg'
+import EyeOff from '../../icons/eye-off.svg'
+import Heart from '../../icons/heart.svg'
 
 const InfoBar = () => (
   <div className={style.infobar}>
-    <i className='material-icons'>fiber_manual_record</i>
-    <i className='material-icons'>share</i>
-    <i className='material-icons'>favorite</i>
-    <i className='material-icons'>visibilty</i>
+      <Circle style={{fill: 'lightgreen'}}/>
+      <Magnet style={{fill: 'var(--Font-color)'}}/>
+      <Heart style={{fill: 'var(--Secondary-color)'}}/>
+      <EyeOff style={{fill: 'var(--Font-color)'}}/>
   </div>
 )
 
